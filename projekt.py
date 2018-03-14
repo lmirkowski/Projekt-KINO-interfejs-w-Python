903d505ecdeaeb60e9336b1e5febc9ae47cb9278
l.mirkowski <mirkowskilukasz@gmail.com>
2018-03-14 13:22:58 +0100

    Dodano logowanie z powitaniem + wybór opcji początkowych usera i admina

--- projekt.py
+++ projekt.py
@@ -10,42 +10,59 @@ class DBConn:
             self.connString()
             perm = self.login()
             if(perm.upper() == 'A'):
-                while(True):
-                    dec = input('1.Select, 2.Delete, 3.Insert, 4.Wyloguj ')
-                    if(dec == '1'):
-                        self.select()
-                    elif(dec == '2'):
-                        self.delete()
-                    elif(dec == '3'):
-                        self.insert()
-                    elif(dec == '4'):
-                        self.connClose()
-                        break
-                    else:
-                        print('Zły wybór')
+                print('---------------------------------------------------------------')
+                print('            ZALOGOWANO JAKO ADMINISTRATOR SYSTEMU              ')
+                print('---------------------------------------------------------------')                
+                self.uprAdmin()
+                
             elif(perm.upper() == 'U'):
-                while(True):
-                    dec = input('1.Wybierz tytuł filmu, 2.Wyloguj ')
-                    if(dec == '1'):
-                        self.selectMovie()
-                        self.showChosenMowie()
-                    elif(dec == '2'):
-                        self.connClose()
-                        break
-                    else:
-                        print('Zły wybór')               
+                print('---------------------------------------------------------------')
+                print('             WITAMY W SYSTEMIE REZERWACJI BILETÓW              ')
+                print('---------------------------------------------------------------')                
+                self.uprUser()
+                            
             else:
                 print('Błąd logowania!')
-        
+                        
     def login(self):
-        login = input('podaj maila: ')
-        password = input('podaj hasło: ')
-        self.c.execute('SELECT perm FROM logowanie WHERE login=%s AND password=%s;', (login, password))
+        email = input('Podaj swój email: ')
+        password = input('Podaj swoje hasło: ')
+        self.c.execute('SELECT perm FROM logowanie WHERE email=%s AND password=%s;', (email, password))
         try:
             perm = self.c.fetchall()[0][0]
         except:
             perm = '0'
         return perm
+    
+    def uprAdmin(self): #wyświetla uprawnienia administratora
+        while(True):
+            dec = input('1.Select, 2.Delete, 3.Insert, 4.Wyloguj ')
+            if(dec == '1'):
+                self.select()
+            elif(dec == '2'):
+                self.delete()
+            elif(dec == '3'):
+                self.insert()
+            elif(dec == '4'):
+                self.connClose()
+                break
+            else:
+                print('Zły wybór ')
+                
+    def uprUser(self): #wyświetla uprawnienia usera - dalej wybór filmu i rezerwacja
+        while(True):
+            dec = input('1.Wybierz tytuł filmu, 2.Wyloguj ')
+            if(dec == '1'):
+                self.selectMovie()
+                self.showChosenMowie()
+                
+            elif(dec == '2'):
+                self.connClose()
+                break
+            else:
+                print('Zły wybór')
+        
+    
     def connString(self):
         self.conn = pymysql.connect('localhost','project','project','kinokopia')
         self.c = self.conn.cursor()
@@ -98,6 +115,7 @@ class DBConn:
             self.c.execute('SELECT idfilm, title, rok_prod, kraj, rezyser, czas_trwania, ogr_wiek, gatunek, obsada, opis FROM film where idfilm=10;')
             for row in self.c.fetchall():
                 print('%2i.| Tytuł: %-35s \n Rok produkcji: %-4i | Kraj: %-15s | Reżyser: %-25s \n Czas trwania: %-10s | Ograniczenie wiekowe %-3s | Gatunek: %-30s | Obsada: %-120s | Opis: %-500s' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]))
+                
         else:
                 print('Podaj poprawny numer filmu')       
         
@@ -122,7 +140,10 @@ class DBConn:
             self.conn.commit()
             self.select()
         except:
-            print('podałeś niepoprawne dane!')
+            print('Podałeś niepoprawne dane! ')
+            
     def connClose(self):
         self.conn.close()
+    
+    
 db = DBConn()
